53eef2e6dc6e010ec9492d45f31c8f15caba4db1
Thiago <trabalho.bandeira@gmail.com>
2022-10-17 18:01:00 -0300

    final commit

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
+
+let foodPrice = 0
+let drinkPrice = 0
+let dessertPrice = 0
+let foodItem = 0
+let drinkItem = 0
+let dessertItem = 0
+let contador = 0
+
 function selectFood(element, getFloatPrice) {
     const prevSelectedFoodItem = document.querySelector('.selected-food')
     if (prevSelectedFoodItem !== null) {
         prevSelectedFoodItem.classList.remove('selected-food');
+        foodItem = 0
     }
     const food = element
     food.classList.add("selected-food")
     //getFloatPrice()
-    let foodPrice = Number.parseFloat(food.querySelector(".food-price").innerHTML).toFixed(2)
+    foodPrice = Number.parseFloat(food.querySelector(".food-price").innerHTML).toFixed(2)
     
 }
 
 function selectDrink(element, getFloatPrice) {
-    const prevSelectedFoodItem = document.querySelector('.selected-drink')
-    if (prevSelectedFoodItem !== null) {
-        prevSelectedFoodItem.classList.remove('selected-drink');
+    const prevSelectedDrinkItem = document.querySelector('.selected-drink')
+    if (prevSelectedDrinkItem !== null) {
+        prevSelectedDrinkItem.classList.remove('selected-drink');
+        drinkItem = 0
     }
     const drink = element
     drink.classList.add("selected-drink")
     //getFloatPrice()
-    let foodPrice = Number.parseFloat(drink.querySelector(".drink-price").innerHTML).toFixed(2)
-    
+    drinkPrice = Number.parseFloat(drink.querySelector(".drink-price").innerHTML).toFixed(2)
+    return(drinkPrice)
 }
 
 function selectDessert(element, getFloatPrice) {
-    const prevSelectedFoodItem = document.querySelector('.selected-dessert')
-    if (prevSelectedFoodItem !== null) {
-        prevSelectedFoodItem.classList.remove('selected-dessert');
+    const prevSelectedDessertItem = document.querySelector('.selected-dessert')
+    if (prevSelectedDessertItem !== null) {
+        prevSelectedDessertItem.classList.remove('selected-dessert');
+        dessertItem = 0
     }
     const dessert = element
     dessert.classList.add("selected-dessert")
     //getFloatPrice()
-    let foodPrice = Number.parseFloat(dessert.querySelector(".dessert-price").innerHTML).toFixed(2)
-    
+    dessertPrice = Number.parseFloat(dessert.querySelector(".dessert-price").innerHTML).toFixed(2)
+    return(dessert)
+
 }
 
-const getFloatPrice = (item, itemClass) => {
-    return Number.parseFloat(item.querySelector(itemClass).innerHTML).toFixed(2)
+
+
+function total(){
+    contador++
+    if(contador > 2){
+        const button = document.querySelector(".btn")
+        button.classList.add("green")
+        const drinP = drinkPrice
+        const desP = dessertPrice
+    }
 }
+
 
 
