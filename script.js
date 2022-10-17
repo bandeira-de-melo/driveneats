2cb96e33ee4a71d5ab964fb1e8d210f286c7d0d3
Thiago <trabalho.bandeira@gmail.com>
2022-10-17 14:36:00 -0300

    add selection functions and style

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-function select(element) {
-    if (element.classList.contains('selected') === false) {
-        element.classList.add('selected');
+function selectFood(element, getFloatPrice) {
+    const prevSelectedFoodItem = document.querySelector('.selected-food')
+    if (prevSelectedFoodItem !== null) {
+        prevSelectedFoodItem.classList.remove('selected-food');
     }
+    const food = element
+    food.classList.add("selected-food")
+    //getFloatPrice()
+    let foodPrice = Number.parseFloat(food.querySelector(".food-price").innerHTML).toFixed(2)
+    
 }
+
+function selectDrink(element, getFloatPrice) {
+    const prevSelectedFoodItem = document.querySelector('.selected-drink')
+    if (prevSelectedFoodItem !== null) {
+        prevSelectedFoodItem.classList.remove('selected-drink');
+    }
+    const drink = element
+    drink.classList.add("selected-drink")
+    //getFloatPrice()
+    let foodPrice = Number.parseFloat(drink.querySelector(".drink-price").innerHTML).toFixed(2)
+    
+}
+
+function selectDessert(element, getFloatPrice) {
+    const prevSelectedFoodItem = document.querySelector('.selected-dessert')
+    if (prevSelectedFoodItem !== null) {
+        prevSelectedFoodItem.classList.remove('selected-dessert');
+    }
+    const dessert = element
+    dessert.classList.add("selected-dessert")
+    //getFloatPrice()
+    let foodPrice = Number.parseFloat(dessert.querySelector(".dessert-price").innerHTML).toFixed(2)
+    
+}
+
+const getFloatPrice = (item, itemClass) => {
+    return Number.parseFloat(item.querySelector(itemClass).innerHTML).toFixed(2)
+}
+
+
